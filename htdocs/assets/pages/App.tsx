a42aeb08aec1c3fb51449fbe1502dcbbd8600314
Bobillier Michel <michel.bobillier@etat.ge.ch>
2024-12-18 16:50:47 +0100

    update to symfony 7.2

--- htdocs/assets/pages/App.tsx
+++ htdocs/assets/pages/App.tsx
@@ -1,32 +1,31 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, createBrowserRouter, RouterProvider } from "react-router-dom";
+import {BrowserRouter, Routes, Route, createBrowserRouter, RouterProvider} from "react-router-dom";
 import Base from "./Base";
 import Home from "./Home";
 
 
-
-
-
 const routes = [
-{
-    path: '/',
-    element:<Base/>
-},
-{
-children: [
-    {
-      path: "/home",
-      element: <Home />,
-   }
-   },
-];
+        {
+            path: '/',
+            element: <Base/>
+        },
+        {
+            children: [
+                {
+                    path: "/home",
+                    element: <Home/>
+                }
+            ]
+        }
+    ]
+;
 
 
-export default function App  ({basename}:{basename:string}) {
+export default function App({basename}: { basename: string }) {
     return (
         <>
-        <i>blalldldld</i>
-        <RouterProvider router={createBrowserRouter(routes,{basename:basename})}   />
+            <i>blalldldld</i>
+            <RouterProvider router={createBrowserRouter(routes, {basename: basename})}/>
         </>
     );
 }
